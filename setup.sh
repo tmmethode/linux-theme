24200282debbc96806df0dc54314bf2604374d93
Methode TWIZEYIMANA <tmmethode1@gmail.com>
2024-04-01 04:27:42 +0200

    fix some bugs and updating

--- setup.sh
+++ setup.sh
@@ -17,30 +17,63 @@ install_configs() {
     mkdir ~/.icons
     mv ~/.local/share/icons/Vimix-cursors/ ~/.icons/
     mv ~/.local/share/icons/Vimix-white-cursors/ ~/.icons/
-    unzip -o extensions.zip -d ~/.local/share/gnome-shell/
-   sudo cp Milkyway.png /usr/local/share/
+        # Get GNOME Shell version
+
+    gnome_shell_version=$(gnome-shell --version | awk '{print $3}')
+    major_version=${gnome_shell_version%%.*}
+
+    # Check major version and execute corresponding file
+
+    if [[ $major_version -eq 40 ]]; then
+    echo "Detected GNOME Shell 40. Executing gnome40.zip"
+    unzip -o gnome40.zip -d ~/.local/share/gnome-shell/
+    elif [[ $major_version -eq 41 ]]; then
+    echo "Detected GNOME Shell 41. Executing gnome41.zip"
+    unzip -o gnome41.zip -d ~/.local/share/gnome-shell/
+    elif [[ $major_version -eq 42 ]]; then
+    echo "Detected GNOME Shell 42. Executing gnome42.zip"
+    unzip -o gnome42.zip -d ~/.local/share/gnome-shell/
+    elif [[ $major_version -eq 43 ]]; then
+    echo "Detected GNOME Shell 43. Executing gnome43.zip"
+    unzip -o gnome43.zip -d ~/.local/share/gnome-shell/
+    elif [[ $major_version -eq 44 ]]; then
+    echo "Detected GNOME Shell 44. Executing gnome44.zip"
+    unzip -o gnome44.zip -d ~/.local/share/gnome-shell/
+    elif [[ $major_version -eq 45 ]]; then
+    echo "Detected GNOME Shell 45. Executing gnome45.zip"
+    unzip -o gnome45.zip -d ~/.local/share/gnome-shell/
+    elif [[ $major_version -eq 46 ]]; then
+    echo "Detected GNOME Shell 46. Executing gnome46.zip"
+    unzip -o gnome46.zip -d ~/.local/share/gnome-shell/
+    else
+    echo "Unsupported GNOME Shell version: $major_version"
+    fi
+    cp Milkyway.png ~/.local/share/gnome-shell
     dconf load /org/gnome/shell/extensions/ < tm.conf
     dconf load / < gnome_tweaks.dconf
+    echo "Logging out......"
+    sleep 5  # Wait for 5 seconds
     gnome-session-quit --logout --no-prompt
 }
 
 # Function to uninstall configurations
 uninstall_configs() {
-    cd ~/.config/tm_setup/
+    dconf load / < ~/.local/share/gnome-shell/backup.dconf
+    rm ~/.local/share/gnome-shell/backup.dconf
     sudo apt remove gnome-tweaks -y
     sudo apt remove chrome-gnome-shell -y
     rm -rf ~/.local/share/fonts
+    rm -rf ~/.config/tm_setup
     rm -rf ~/.local/share/gnome-shell/extensions/arcmenu@arcmenu.com
     rm -rf ~/.local/share/gnome-shell/extensions/blur-my-shell@aunetx
     rm -rf ~/.local/share/gnome-shell/extensions/CoverflowAltTab@palatis.blogspot.com
     rm -rf ~/.local/share/gnome-shell/extensions/dash-to-panel@jderose9.github.com
     rm -rf ~/.local/share/gnome-shell/extensions/gsconnect@andyholmes.github.io
-    rm -rf ~/.local/share/gnome-shell/extensions/mediacontrols@cliffniff.github.com
     rm -rf ~/.local/share/gnome-shell/extensions/user-theme@gnome-shell-extensions.gcampax.github.com 
-    dconf reset -f /org/gnome/
-    dconf load / < backup.dconf 
-    rm -rf ~/.config/tm_setup/ 
-   gnome-session-quit --logout --no-prompt
+
+    echo "Logging out......"
+    sleep 5  # Wait for 5 seconds
+    gnome-session-quit --logout --no-prompt
 }
 
 # Main script
@@ -48,26 +81,58 @@ echo "Welcome to setup script!"
 echo "Choose an option:"
 echo "1. Install configurations"
 echo "2. Uninstall configurations"
+
+echo -n "Choice: "
 read choice
 
+gnome_shell_minor_version=$(gnome-shell --version | awk '{printf "%.2f\n", $3}')
+gnome_shell_version=$(gnome-shell --version | awk '{print $3}')
+major_version=${gnome_shell_version%%.*}
 case "$choice" in
     1)
-       # Check if tm_setup.zip exists, if not, download it
-        if [ ! -f "tm_setup.zip" ]; then
-            echo "Downloading setup..."
-            wget https://raw.githubusercontent.com/tmmethode/linux-theme/main/tm_setup.zip
-        fi
-        # Check again if tm_setup.zip exists
-        if [ -f "tm_setup.zip" ]; then
-            install_configs
-        else
-            echo "Failed to download setup. Exiting."
-        fi
-        ;;
+        if [[ $major_version -eq 40 || $major_version -eq 41 || $major_version -eq 42 || $major_version -eq 43 || $major_version -eq 44 || $major_version -eq 45 || $major_version -eq 46 ]]; then
+        # Handle versions 40, 41, and 42, .. as a group
+        echo "Detected a supported GNOME Shell version $major_version" 
+            echo "Executing setup.sh"
+            sleep 5  # Wait for 5 seconds
+            # Check if tm_setup.zip exists, if not, download it
+                if [ -d ~/.config/tm_setup ]; then
+                echo "Setup Already installed!"
+                echo "Terminating..."
+                sleep 3
+                exit 1
+
+                elif [ ! -f "tm_setup.zip" ]; then
+                    echo "Downloading setup..."
+                    wget  https://example.com/tm_setup.zip
+                fi
+                # Check again if tm_setup.zip exists
+                if [ -f "tm_setup.zip" ]; then
+                    install_configs
+                else
+                    echo "Failed to download setup. Exiting."
+                fi
+
+        else 
+                echo "Unsupported GNOME Shell version $major_version.  Terminating..."
+                sleep 3
+                fi
+                ;;
     2)
-        uninstall_configs
+                    # Check again if folder exists
+        if [ -d ~/.config/tm_setup ]; then
+                echo "Uninstalling....."
+                sleep 4
+                uninstall_configs
+        else
+                echo "Not exist in your system!. Exiting."
+                sleep 3
+        fi
         ;;
     *)
         echo "Invalid choice. Please type '1' for installation or '2' for uninstallation."
         ;;
 esac
+
+
+
